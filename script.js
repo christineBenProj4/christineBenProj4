dd97729f4597b9ec327c18b24a0163f089a7acaf
christineshiels <christineshiels@gmail.com>
2020-11-17 20:00:47 -0800

    API request returning proper results :boom: :sparkles: :dizzy:

--- script.js
+++ script.js
@@ -1,62 +1,35 @@
-// AAAAAAAAHHHHHHHH!!!!!!!!!!!!!
 
+// namespace
 const mapApp = {};
 
-mapApp.apiKey ='Tfj4LjgSABueLr8yE5j99mdAMj3Fgspu';
-
-mapApp.searchUrl='http://www.mapquestapi.com/search/v2/radius';
-
-mapApp.mapUrl= 'https://www.mapquestapi.com/staticmap/v5/map';
-
-mapApp.units = 'wmin';
-mapApp.radius = '15';
-mapApp.origin = '483 Queen St W 3rd floor, Toronto, ON M5V 2A9';
-mapApp.lat = '59.566601';
-mapApp.long = '-98.6166';
+mapApp.apiKey = 'Tfj4LjgSABueLr8yE5j99mdAMj3Fgspu';
+mapApp.searchUrl = 'http://www.mapquestapi.com/search/v2/radius';
+mapApp.origin = '1931 Nelson St Vancouver BC';
 
 
-
-
-mapApp.getInfo = (function() {
+mapApp.searchResults = function() {
     $.ajax({
-        url: mapApp.searchUrl,
-        method: 'GET',
-        dataType: 'json',
+    url: mapApp.searchUrl,
+    method: 'GET',
+    dataType: 'json',
+    data: {
         origin: mapApp.origin,
-        data: {
-            units: mapApp.units,
-            radius: mapApp.radius,
-            key: mapApp.apiKey,
-        }
-    }).then(function(searchData) {
-        console.log(searchData);
-        
-    })
+        key: mapApp.apiKey,
+    }
+}).then(function(results) {
+    console.log(results);
+    mapApp.userLat = results.origin.latLng.lat;
+    mapApp.userLong = results.origin.latLng.lng;
 })
+};
 
 
-
-// the following was copied from the Mapquest API docs for displaying dynamic maps on the page.
-L.mapquest.key = 'Tfj4LjgSABueLr8yE5j99mdAMj3Fgspu';
-
-// 'map' refers to a <div> element with the ID map
-L.mapquest.map('map', {
-    center: [mapApp.lat, mapApp.long],
-    layers: L.mapquest.tileLayer('map'),
-    zoom: 4
-
-});
-
-
-
-mapApp.init = () => {
-    console.log('init!');
-    mapApp.getInfo();
-}
-
+mapApp.init = function() {
+    console.log('init');
+    mapApp.searchResults();
+};
 
 $(function() {
     console.log('ready!');
     mapApp.init();
-
 })
