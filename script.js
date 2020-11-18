39a97eae46b17fd1b0bd6a361ee763ff0be8f167
christineshiels <christineshiels@gmail.com>
2020-11-17 20:11:59 -0800

    Map displaying with correct coordinates  :globe_with_meridians: :earth_africa: :metal:

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const mapApp = {};
 
 mapApp.apiKey = 'Tfj4LjgSABueLr8yE5j99mdAMj3Fgspu';
 mapApp.searchUrl = 'http://www.mapquestapi.com/search/v2/radius';
-mapApp.origin = '1931 Nelson St Vancouver BC';
+mapApp.origin = '1504 Victoria Ave Saskatoon SK';
 
 
 mapApp.searchResults = function() {
@@ -20,6 +20,19 @@ mapApp.searchResults = function() {
     console.log(results);
     mapApp.userLat = results.origin.latLng.lat;
     mapApp.userLong = results.origin.latLng.lng;
+    
+
+L.mapquest.key = mapApp.apiKey;
+
+// 'map' refers to a <div> element with the ID map
+L.mapquest.map('map', {
+  center: [mapApp.userLat, mapApp.userLong],
+  // L.mapquest.tileLayer('map') is the MapQuest map tile layer
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+
+
 })
 };
 
